eeb84850be9897d4a5e68e3c7def8260f11fd3c5
jjyrkii <111977699+jjyrkii@users.noreply.github.com>
2023-03-19 13:19:15 +0100

    besser wirds net

--- resources/views/departments/trainings.blade.php
+++ resources/views/departments/trainings.blade.php
@@ -28,11 +28,30 @@
                 <td>{{ \App\Models\Department::find($training->department_id)->name }}</td>
                 <td>
                     <form action="{{ route('trainings.destroy', $training->id) }}" method="POST">
-                        <a class="btn btn-info" href="{{ route('trainings.show', $training->id) }}">Show</a>
-                        <a class="btn btn-primary" href="{{ route('trainings.edit', $training->id) }}">Edit</a>
+                        <a class="btn btn-dark" href="{{ route('trainings.show', $training->id) }}">Anzeigen</a>
+                        <a class="btn btn-dark" href="{{ route('trainings.edit', $training->id) }}">Bearbeiten</a>
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="btn btn-danger">Delete</button>
+                        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Löschen</button>
+                        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Löschen bestätigen</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <span>Sind Sie sicher?</span>
+                                        <br/>
+                                        <span>Das Training wird unwiderruflich gelöscht!</span>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Abbrechen</button>
+                                        <button type="submit" class="btn btn-danger">Löschen</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </form>
                 </td>
             </tr>
